51714d1d87b0ccc56d42248ac27da9e1b02bf4e6
Vinicius Rodrigues <vinior@ime.usp.br>
2026-02-15 12:01:52 -0300

    Add initial project files including .gitignore, composer.json, composer.lock, and HtmlRenderer class

---
+++ src/HtmlRenderer.php
@@ -0,0 +1,123 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vinicius\HtmlRenderer;
+
+use League\Plates\Engine;
+use League\Plates\Template\Template;
+
+class HtmlRenderer
+{
+    private Template $template;
+    private string $language;
+    private string $templatePath;
+    private string $viewsPath;
+    private array $templateData = [];
+    private array $headerData = [];
+    private array $footerData = [];
+    private array $contentData = [];
+    private array $headData = [];
+    private array $commonData = [];
+    private string $body;
+    private string $contentFolder;
+    private string $header;
+    private string $footer;
+    private string $head;
+
+    function __construct(string $templatePath, string $viewsPath, string $templateFile='template')
+    {
+        $engine = new Engine($templatePath);
+        $engine->addFolder('views', $viewsPath);
+        $this->viewsPath = $viewsPath;
+        $this->template = $engine->make($templateFile);
+        $this->templatePath = $templatePath;
+        $this->setBody('body');
+        $this->setHeader('header');
+        $this->setFooter('footer');
+        $this->setHead('head');
+    }
+    
+    public function setBody(string $file): void
+    {
+        $this->body = 'views::'.$file.'/content';
+        $this->contentFolder = $this->viewsPath.$file.'/';
+    }
+    public function setLanguage(string $language): void
+    {
+        $this->language = $language;
+        $this->template->data(require $this->templatePath."template.{$language}.php");
+    }
+    public function setHeader(string $file):void
+    {
+        $this->header = 'headers/'.$file;
+    }
+    public function setFooter(string $file):void
+    {
+        $this->footer = 'footers/'.$file;
+    }
+    public function setHead(string $file):void
+    {
+        $this->head = 'heads/'.$file;
+    }
+    
+    public function addHeadData(array $data):void
+    {
+        $this->headData = array_merge($this->headData, $data);
+    }
+    public function addCommonData(array $data):void
+    {
+        $this->commonData = array_merge($this->commonData, $data);
+    }
+    public function addTemplateData(array $data):void
+    {
+        $this->templateData = array_merge($this->templateData, $data);
+    }
+    public function addHeaderData(array $data):void
+    {
+        $this->headerData =  array_merge($this->headerData, $data);
+    }
+    public function addFooterData(array $data):void
+    {
+        $this->footerData =  array_merge($this->footerData, $data);
+    }
+    public function addContentData(array $data):void
+    {
+        $this->contentData =  array_merge($this->contentData, $data);
+    }
+    public function render(): string
+    {
+        $this->addTemplateData(require $this->templatePath.'template.'.$this->language.'.php');
+        $this->retrieveContentData();
+        $head__data=$this->headData+$this->templateData+$this->commonData;
+        $header__data=$this->headerData+$this->templateData+$this->commonData;
+        $footer__data=$this->footerData+$this->templateData+$this->commonData;
+        $content__data=$this->contentData+$this->commonData;
+        return $this->template->render(
+            [
+            'template__head'=>$this->head,
+            'template__header'=>$this->header,
+            'template__footer'=>$this->footer,
+            'template__content'=>$this->body,
+            ] +
+            $this->commonData+$this->templateData +
+            [
+            'template__head__data'=>$head__data,
+            'template__header__data'=>$header__data,
+            'template__footer__data'=>$footer__data,
+            'template__content__data'=>$content__data,
+        ]);
+    }
+    protected function retrieveContentData(): void
+    {
+        $this->addCommonData($this->retrieveFileData($this->contentFolder.'locale.'.$this->language.'.php'));
+        $this->addHeadData($this->retrieveFileData($this->contentFolder.'data.head.php'));
+    }
+    protected function retrieveFileData(string $filePath): array
+    {
+        if (file_exists($filePath)) {
+            return require $filePath;
+        }
+        return [];
+    }
+}
